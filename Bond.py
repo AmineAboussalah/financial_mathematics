2123ef13c431377cf98f4bdeb6958fe0be3d8385
louisdv75 <louisdevitry@gmail.com>
2018-05-15 22:38:44 +0200

    Update

--- Bond.py
+++ Bond.py
@@ -16,4 +16,27 @@ class BondOnePeriod:
         """
         self.r = float(r)
         self.initial_value = 1.
-        self.final_value = self.initial_value*(1.+self.r)
+        self.final_value = np.round(self.initial_value*(1.+self.r), 2)
+
+
+class BondMultiPeriod:
+
+    """
+    Parameters:
+    - r: (Float) interest rate of the bond
+    - T: (int) period
+
+    Attributes:
+    - initial_value: (Float)
+    - final_value: (Float)
+    """
+
+    def __init__(self, r, T):
+        self.r = r
+        self.T = T
+        self.values = [None]*T
+
+        self.values[0] = 1
+
+        for i in range(1, T):
+            self.values[i] = self.values[i-1]*(1+self.r)
