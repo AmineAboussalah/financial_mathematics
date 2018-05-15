e188e9f9d1d94f089c04f1979a86ffa39d4a30d4
louisdv75 <louisdevitry@gmail.com>
2018-05-15 22:47:18 +0200

    Error correction

--- Stock.py
+++ Stock.py
@@ -40,7 +40,6 @@ class StockOnePeriod:
         # Compute the derivative
         self.derivative = np.maximum(self.final_value-self.k, 0)
         return(self.derivative)
-
 class StockMultiPeriod:
     """
     In this class, we only consider (at the moment) European call option.
@@ -50,51 +49,6 @@ class StockMultiPeriod:
     - u, d: (upward factor, downward factor) possible values at time t=1
     - p: probability that the upward factor is activated
 
-    Attributes:
-    - initial_value: initial value of the stock at time t = 0
-    - final_value: possible values of the stock at time t = 1
-    - k: strike price of the option (is defined)
-    - derivative: payoffs at time t = 1 (with respect to k)
-    """
-    def __init__(self, s, u, d, p, T):
-        """
-        Set the parameters s, u, p,  d and compute the initial and final values.
-        """
-        # Define the binomial model
-        self.u = float(u)
-        self.d = float(d)
-        self.p = float(p)
-        self.T = T
-
-        # Compute initial and final values
-        self.values = [None]*T
-        self.values[0] = s
-
-        for i in range(1, T):
-            self.values[i] = np.asarray([self.u * self.values[i-1], \
-                                       self.d * self.values[i-1]])
-
-    def compute_derivative(self, k):
-       """
-       The derivative is the difference between the possible values of the stock
-       at time t = 1 and the strike price.
-       Careful: Valid only for European call option
-       """
-       # Define the strike price at which the option exercices
-       self.k = k
-       # Compute the derivative
-       self.derivative = np.maximum(self.values[self.T]-self.k, 0)
-       return(self.derivative)
-
-class StockMultiPeriod:
-    """
-    In this class, we only consider (at the moment) European call option.
-​
-    Parameters:
-    - s: initial value of the stock
-    - u, d: (upward factor, downward factor) possible values at time t=1
-    - p: probability that the upward factor is activated
-​
     Attributes:
     - initial_value: initial value of the stock at time t = 0
     - final_value: possible values of the stock at time t = 1
@@ -110,11 +64,11 @@ class StockMultiPeriod:
         self.d = float(d)
         self.p = float(p)
         self.T = T
-​
+
         # Compute initial and final values
         self.values = [None]*(T+1)
         self.values[0] = s
-​
+
         for i in range(1, T+1):
             self.values[i] = np.asarray([self.u * self.values[i-1], \
                                        self.d * self.values[i-1]])
